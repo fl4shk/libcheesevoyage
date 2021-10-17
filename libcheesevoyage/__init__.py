92bb64ae75f96605427b79de67ae7da9edc615d3
fl4shk <fl4shk@users.noreply.github.com>
2021-10-17 11:33:59 -0500

    test lack of relative import

--- libcheesevoyage/__init__.py
+++ libcheesevoyage/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from .general import *
-from .math import *
-from .misc_util import *
-from .misc_util_running import *
+from general import *
+from math import *
+from misc_util import *
+from misc_util_running import *
