562924c7fd81a559283ff8f6c8d58baa66841608
fl4shk <fl4shk@users.noreply.github.com>
2021-10-17 11:21:06 -0500

    `setup.py`: flesh out this file

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
 from setuptools import setup
+
+setup \
+(
+	name="libcheesevoyage",
+	version="0.1"
+	description="FL4SHK's personal nMigen library",
+	url="https://github.com/fl4shk/libcheesevoyage",
+	author="Andrew Clark (FL4SHK)",
+	author_email="fl4shk@users.noreply.github.com",
+	license="MIT",
+	packages=["libcheesevoyage"],
+	zip_safe=False,
+)
