4adc70de75932fbefe51740defacdad8f8d14bfb
fl4shk <fl4shk@users.noreply.github.com>
2023-06-03 13:19:17 -0500

    don't import as much

--- libcheesevoyage/__init__.py
+++ libcheesevoyage/__init__.py
@@ -1,8 +1,8 @@
 from libcheesevoyage.misc_util import *
 from libcheesevoyage.misc_util_running import *
 
-from libcheesevoyage.general import *
-from libcheesevoyage.gfx import *
-from libcheesevoyage.intrcn_lcv import *
-from libcheesevoyage.math_lcv import *
-from libcheesevoyage.mem import *
+#from libcheesevoyage.general import *
+#from libcheesevoyage.gfx import *
+#from libcheesevoyage.intrcn_lcv import *
+#from libcheesevoyage.math_lcv import *
+#from libcheesevoyage.mem import *
