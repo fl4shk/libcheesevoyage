2cdc110a14f27619bc93ee321bda76a08d751c0a
fl4shk <fl4shk@users.noreply.github.com>
2023-09-09 14:55:11 -0500

    `hw/spinal/libcheesevoyage/gfx/lcvVgaCtrlMod.scala`: possibly finish translating old code to SpinalHDL

--- tmp/job_1/unamed.sv
+++ tmp/job_1/unamed.sv
@@ -1,6 +1,6 @@
 // Generator : SpinalHDL v1.9.3    git head : 029104c77a54c53f1edda327a3bea333f7d65fd9
 // Component : unamed
-// Git hash  : 9db38541cc1b0c9738a26da2d4c34c6a2c182071
+// Git hash  : 453190cf97277f2dc6e762a573f2b5d9f43acc87
 
 `timescale 1ns/1ps
 
