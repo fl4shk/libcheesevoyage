916c4723e55404d104145d546bb9afad8cf69060
fl4shk <fl4shk@users.noreply.github.com>
2023-10-06 16:46:39 -0500

    `hw/spinal/libcheesevoyage/gfx/gpu2dMod.scala`: more progress

--- hw/spinal/libcheesevoyage/gfx/gpu2dMod.scala
+++ hw/spinal/libcheesevoyage/gfx/gpu2dMod.scala
@@ -36,7 +36,8 @@ case class Gpu2dParams(
                               // This is used to duplicate generated pixels 
                               // so that the generated video signal can 
                               // fill the screen.
-  tileSize2d: ElabVec2[Int],  // width/height of a tile (in pixels)
+  tileSize2dPow: ElabVec2[Int],  // power of two for width/height of a tile 
+                              // (in pixels)
   numTilesPow: Int,           // power of two for total number of tiles
                               // (how much memory to reserve for tiles)
   bgSize2dInTiles: ElabVec2[Int], // width/height of a background
@@ -51,12 +52,16 @@ case class Gpu2dParams(
   //--------
   tileColArrRamStyle: String="block",
   bgEntryArrRamStyle: String="block",
-  bgAttrsArrRamStyle: String="block",
+  //bgAttrsArrRamStyle: String="block",
   objAttrsArrRamStyle: String="block",
   palEntryArrRamStyle: String="block",
   lineArrRamStyle: String="block",
 ) {
   //--------
+  def tileSize2d = ElabVec2[Int](
+    x=1 << tileSize2dPow.x,
+    y=1 << tileSize2dPow.y,
+  )
   def numTiles = 1 << numTilesPow
   def numBgs = 1 << numBgsPow
   def numObjs = 1 << numObjsPow
@@ -70,6 +75,8 @@ case class Gpu2dParams(
 
   //def halfLineMemSize = intnlFbSize2d.x
   //def lineMemSize = intnlFbSize2d.x * 2
+  def lineMemSize = intnlFbSize2d.x
+  def numLineMems = 2
   //--------
   def numPxsPerTile = tileSize2d.x * tileSize2d.y
   def numPxsForAllTiles = numTiles * numPxsPerTile
@@ -92,7 +99,7 @@ case class Gpu2dParams(
   def tileColMemIdxWidth = log2Up(numPxsForAllTiles)
 
   def bgEntryMemIdxWidth = log2Up(numTilesPerBg)
-  def bgAttrsMemIdxWidth = log2Up(numBgs)
+  //def bgAttrsMemIdxWidth = log2Up(numBgs)
 
   def palEntryMemIdxWidth = log2Up(numColsInPal)
   //def tilePixelIdxWidth = log2Up(numPxsForAllTiles)
@@ -122,7 +129,8 @@ object DefaultGpu2dParams {
       x=4, // 1920 / 4 = 480
       y=4, // 1080 / 4 = 270
     ),
-    tileSize2d: ElabVec2[Int]=ElabVec2[Int](x=8, y=8),
+    //tileSize2d: ElabVec2[Int]=ElabVec2[Int](x=8, y=8),
+    tileSize2dPow: ElabVec2[Int]=ElabVec2[Int](x=log2Up(8), y=log2Up(8)),
     numBgsPow: Int=log2Up(4),
     numObjsPow: Int=log2Up(256),
     //numObjsPerScanline: Int=64,
@@ -130,61 +138,67 @@ object DefaultGpu2dParams {
     //--------
     tileColArrRamStyle: String="block",
     bgEntryArrRamStyle: String="block",
-    bgAttrsArrRamStyle: String="block",
+    //bgAttrsArrRamStyle: String="block",
     objAttrsArrRamStyle: String="block",
     palEntryArrRamStyle: String="block",
     lineArrRamStyle: String="block",
     //--------
-  ) = Gpu2dParams(
-    //rgbConfig=RgbConfig(rWidth=6, gWidth=6, bWidth=6),
-    //intnlFbSize2d=ElabVec2[Int](
-    //  //x=(1920 / 3).toInt, // 640
-    //  //y=(1080 / 3).toInt, // 360
-    //  x=(1920 / 4).toInt, // 480
-    //  y=(1080 / 4).toInt, // 270
-    //),
-    //physFbSize2dScale=ElabVec2[Int](
-    //  //x=3, // 1920 / 4 = 640
-    //  //y=3, // 1080 / 3 = 360
-    //  x=4, // 1920 / 4 = 480
-    //  y=4, // 1080 / 4 = 270
-    //),
-    //tileSize2d=ElabVec2[Int](x=8, y=8),
-    rgbConfig=rgbConfig,
-    intnlFbSize2d=intnlFbSize2d,
-    physFbSize2dScale=physFbSize2dScale,
-    tileSize2d=tileSize2d,
-    numTilesPow=(
-      //1024
-      //2048 // (480 * 270) / (8 * 8) = 2025
-      //1 <<
-      log2Up(
-        (intnlFbSize2d.x * intnlFbSize2d.y)
-        / (tileSize2d.x * tileSize2d.y)
-      )
-    ),
-    //bgSize2dInTiles=ElabVec2[Int](x=128, y=128),
-    bgSize2dInTiles=ElabVec2[Int](
-      x=(1 << log2Up(intnlFbSize2d.x / tileSize2d.x)),
-      y=(1 << log2Up(intnlFbSize2d.y / tileSize2d.y)),
-    ),
-    //numBgs=4,
-    //numObjs=256,
-    ////numObjsPerScanline=64,
-    //numColsInPal=256,
-    numBgsPow=numBgsPow,
-    numObjsPow=numObjsPow,
-    //numObjsPerScanline=numObjsPerScanline,
-    numColsInPalPow=numColsInPalPow,
-    //--------
-    tileColArrRamStyle=tileColArrRamStyle,
-    bgEntryArrRamStyle=bgEntryArrRamStyle,
-    bgAttrsArrRamStyle=bgAttrsArrRamStyle,
-    objAttrsArrRamStyle=objAttrsArrRamStyle,
-    palEntryArrRamStyle=palEntryArrRamStyle,
-    lineArrRamStyle=lineArrRamStyle,
-    //--------
-  )
+  ) = {
+    def tileSize2d = ElabVec2[Int](
+      x=1 << tileSize2dPow.x,
+      y=1 << tileSize2dPow.y,
+    )
+    Gpu2dParams(
+      //rgbConfig=RgbConfig(rWidth=6, gWidth=6, bWidth=6),
+      //intnlFbSize2d=ElabVec2[Int](
+      //  //x=(1920 / 3).toInt, // 640
+      //  //y=(1080 / 3).toInt, // 360
+      //  x=(1920 / 4).toInt, // 480
+      //  y=(1080 / 4).toInt, // 270
+      //),
+      //physFbSize2dScale=ElabVec2[Int](
+      //  //x=3, // 1920 / 4 = 640
+      //  //y=3, // 1080 / 3 = 360
+      //  x=4, // 1920 / 4 = 480
+      //  y=4, // 1080 / 4 = 270
+      //),
+      //tileSize2d=ElabVec2[Int](x=8, y=8),
+      rgbConfig=rgbConfig,
+      intnlFbSize2d=intnlFbSize2d,
+      physFbSize2dScale=physFbSize2dScale,
+      tileSize2dPow=tileSize2dPow,
+      numTilesPow=(
+        //1024
+        //2048 // (480 * 270) / (8 * 8) = 2025
+        //1 <<
+        log2Up(
+          (intnlFbSize2d.x * intnlFbSize2d.y)
+          / (tileSize2d.x * tileSize2d.y)
+        )
+      ),
+      //bgSize2dInTiles=ElabVec2[Int](x=128, y=128),
+      bgSize2dInTiles=ElabVec2[Int](
+        x=(1 << log2Up(intnlFbSize2d.x / tileSize2d.x)),
+        y=(1 << log2Up(intnlFbSize2d.y / tileSize2d.y)),
+      ),
+      //numBgs=4,
+      //numObjs=256,
+      ////numObjsPerScanline=64,
+      //numColsInPal=256,
+      numBgsPow=numBgsPow,
+      numObjsPow=numObjsPow,
+      //numObjsPerScanline=numObjsPerScanline,
+      numColsInPalPow=numColsInPalPow,
+      //--------
+      tileColArrRamStyle=tileColArrRamStyle,
+      bgEntryArrRamStyle=bgEntryArrRamStyle,
+      //bgAttrsArrRamStyle=bgAttrsArrRamStyle,
+      objAttrsArrRamStyle=objAttrsArrRamStyle,
+      palEntryArrRamStyle=palEntryArrRamStyle,
+      lineArrRamStyle=lineArrRamStyle,
+      //--------
+    )
+  }
 }
 
 case class Gpu2dTileCol(
@@ -201,6 +215,9 @@ case class Gpu2dTileColStmPayload(
   //--------
   //val rgb = Rgb(params.rgbConfig)
   val tileCol = Gpu2dTileCol(params=params)
+
+  //// the BG priority for for the whole tile
+  //val wholeTilePrio = UInt(log2Up(params.numBgs) bits)
 
   // `Mem` index, so in units of pixels
   //val idx = Vec2(LcvVgaCtrlMiscIo.coordElemT())
@@ -229,7 +246,7 @@ case class Gpu2dObjAttrsStmPayload(
   params: Gpu2dParams,
 ) extends Bundle {
   //--------
-  val objAttr = Gpu2dObjAttrs(params=params)
+  val objAttrs = Gpu2dObjAttrs(params=params)
   // `Mem` index
   val memIdx = UInt(params.objAttrsMemIdxWidth bits)
   //--------
@@ -262,15 +279,16 @@ case class Gpu2dBgAttrs(
 ) extends Bundle {
   //--------
   val scroll = params.bgPxCoordT()
+  //val prio = UInt(log2Up(params.numBgs) bits)
   //--------
 }
 case class Gpu2dBgAttrsStmPayload(
   params: Gpu2dParams,
 ) extends Bundle {
   //--------
-  val attrs = Gpu2dBgAttrs(params=params)
+  val bgAttrs = Gpu2dBgAttrs(params=params)
 
-  val memIdx = UInt(params.bgAttrsMemIdxWidth bits)
+  //val memIdx = UInt(params.bgAttrsMemIdxWidth bits)
   //--------
 }
 
@@ -311,8 +329,18 @@ case class Gpu2dIo(
   //val tilePush = slave Stream()
   //val tilePush = slave Stream()
   val tileColPush = slave Stream(Gpu2dTileColStmPayload(params=params))
-  val bgEntryPush = slave Stream(Gpu2dBgEntryStmPayload(params=params))
-  val bgAttrsPush = slave Stream(Gpu2dBgAttrsStmPayload(params=params))
+  val bgEntryPushArr = new ArrayBuffer[Stream[Gpu2dBgEntryStmPayload]]()
+  val bgAttrsPushArr = new ArrayBuffer[Stream[Gpu2dBgAttrsStmPayload]]()
+  for (idx <- 0 to params.numBgs - 1) {
+    bgEntryPushArr += (
+      slave Stream(Gpu2dBgEntryStmPayload(params=params))
+        .setName(f"bgEntryPushArr_$idx")
+    )
+    bgAttrsPushArr += (
+      slave Stream(Gpu2dBgAttrsStmPayload(params=params))
+        .setName(f"bgAttrsPushArr_$idx")
+    )
+  }
   val objAttrsPush = slave Stream(Gpu2dObjAttrsStmPayload(params=params))
   val palEntryPush = slave Stream(Gpu2dPalEntryStmPayload(params=params))
   //--------
@@ -320,50 +348,67 @@ case class Gpu2dIo(
   //--------
 }
 case class Gpu2d(
-  params: Gpu2dParams,
+  params: Gpu2dParams=DefaultGpu2dParams(),
 ) extends Component {
   //--------
   val io = Gpu2dIo(params=params)
   //--------
   val tileColPush = io.tileColPush
-  val bgEntryPush = io.bgEntryPush
-  val bgAttrsPush = io.bgAttrsPush
+  tileColPush.ready := True
+  val bgEntryPushArr = io.bgEntryPushArr
+  val bgAttrsPushArr = io.bgAttrsPushArr
+  for (idx <- 0 to params.numBgs - 1) {
+    bgEntryPushArr(idx).ready := True
+    bgAttrsPushArr(idx).ready := True
+  }
   val objAttrsPush = io.objAttrsPush
+  objAttrsPush.ready := True
   val palEntryPush = io.palEntryPush
+  palEntryPush.ready := True
   //--------
   val pop = io.pop
-  val col = pop.payload.col
-  val physPxPos = pop.payload.physPxPos
-  val intnlPxPos = pop.payload.intnlPxPos
-  val tilePos = pop.payload.tilePos
+  val rPopValid = Reg(Bool()) init(False)
+  val rPopPayload = Reg(Gpu2dPopPayload(params=params))
+  rPopPayload.init(rPopPayload.getZero)
+  //pop.valid := True
+  //val col = pop.payload.col
+  //val physPxPos = pop.payload.physPxPos
+  //val intnlPxPos = pop.payload.intnlPxPos
+  //val tilePos = pop.payload.tilePos
   //--------
   val loc = new Area {
     //--------
     //val nextCol = KeepAttribute(cloneOf(.payload.col))
-    val rPastCol = KeepAttribute(RegNext(col))
-    rPastCol.init(rPastCol.getZero)
-    //--------
-    //val nextPhysPxPos = KeepAttribute(cloneOf(.payload.physPxPos))
-    val rPastPhysPxPos = KeepAttribute(RegNext(physPxPos))
-    rPastPhysPxPos.init(rPastPhysPxPos.getZero)
-    //--------
-    //val nextIntnlPxPos = KeepAttribute(cloneOf(IntnlPxPos))
-    val rPastIntnlPxPos = KeepAttribute(RegNext(intnlPxPos))
-    rPastIntnlPxPos.init(rPastIntnlPxPos.getZero)
-    //--------
-    //val nextTilePos = KeepAttribute(cloneOf(.payload.tilePos))
-    val rPastTilePos = KeepAttribute(RegNext(tilePos))
-    rPastTilePos.init(rPastTilePos.getZero)
+    //val rPastCol = KeepAttribute(RegNext(col))
+    //rPastCol.init(rPastCol.getZero)
+    ////--------
+    ////val nextPhysPxPos = KeepAttribute(cloneOf(.payload.physPxPos))
+    //val rPastPhysPxPos = KeepAttribute(RegNext(physPxPos))
+    //rPastPhysPxPos.init(rPastPhysPxPos.getZero)
+    ////--------
+    ////val nextIntnlPxPos = KeepAttribute(cloneOf(IntnlPxPos))
+    //val rPastIntnlPxPos = KeepAttribute(RegNext(intnlPxPos))
+    //rPastIntnlPxPos.init(rPastIntnlPxPos.getZero)
 
-    val nextTilePosPlus1Overflow = Vec2(Bool())
-    val rTilePosPlus1Overflow = RegNext(nextTilePosPlus1Overflow).init( 
-      MkVec2(
-        dataType=Bool(),
-        x=False,
-        y=False,
-      )
-    )
+    //val nextIntnlPxPosPlus1Overflow = Vec2(Bool())
+    //val rIntnlPxPosPlus1Overflow = RegNext(nextIntnlPxPosPlus1Overflow)
+    //  .init(MkVec2(
+    //    Bool(),
+    //    x=False,
+    //    y=False
+    //  ))
+    ////--------
+    ////val nextTilePos = KeepAttribute(cloneOf(.payload.tilePos))
+    //val rPastTilePos = KeepAttribute(RegNext(tilePos))
+    //rPastTilePos.init(rPastTilePos.getZero)
 
+    //val nextTilePosPlus1Overflow = Vec2(Bool())
+    //val rTilePosPlus1Overflow = RegNext(nextTilePosPlus1Overflow)
+    //  .init(MkVec2(
+    //    dataType=Bool(),
+    //    x=False,
+    //    y=False,
+    //  ))
     //--------
     val tileColMem = Mem(
       //wordType=UInt(params.palEntryMemIdxWidth bits),
@@ -372,6 +417,12 @@ case class Gpu2d(
     )
       .initBigInt(Array.fill(params.numPxsForAllTiles)(BigInt(0)).toSeq)
       .addAttribute("ram_style", params.tileColArrRamStyle)
+    when (tileColPush.fire) {
+      tileColMem.write(
+        address=tileColPush.payload.memIdx,
+        data=tileColPush.payload.tileCol,
+      )
+    }
     //--------
     //val bgEntryMem = Mem(
     //  wordType=Gpu2dBgEntry(params=params),
@@ -380,21 +431,37 @@ case class Gpu2d(
     //  .addAttribute("ram_style", params.palEntryArrRamStyle)
     //--------
     val bgEntryMemArr = new ArrayBuffer[Mem[Gpu2dBgEntry]]()
+    val bgAttrsArr = new ArrayBuffer[Gpu2dBgAttrs]()
     for (idx <- 0 to params.numBgs - 1) {
+      //--------
       bgEntryMemArr += Mem(
         wordType=Gpu2dBgEntry(params=params),
         wordCount=params.numTilesPerBg,
       )
         .initBigInt(Array.fill(params.numTilesPerBg)(BigInt(0)).toSeq)
-        .addAttribute("ram_style", params.palEntryArrRamStyle)
+        .addAttribute("ram_style", params.bgEntryArrRamStyle)
         .setName(f"bgEntryMemArr_$idx")
+      when (bgEntryPushArr(idx).fire) {
+        bgEntryMemArr(idx).write(
+          address=bgEntryPushArr(idx).payload.memIdx,
+          data=bgEntryPushArr(idx).payload.bgEntry,
+        )
+      }
+      //--------
+      bgAttrsArr += Gpu2dBgAttrs(params=params)
+      bgAttrsArr(idx).init(bgAttrsArr(idx).getZero)
+        .setName(f"bgAttrsArr_$idx")
+      when (bgAttrsPushArr(idx).fire) {
+        bgAttrsArr(idx) := bgAttrsPushArr(idx).payload.bgAttrs
+      }
+      //--------
     }
-    val bgAttrsMem = Mem(
-      wordType=Gpu2dBgAttrs(params=params),
-      wordCount=params.numBgs,
-    )
-      .initBigInt(Array.fill(params.numBgs)(BigInt(0)).toSeq)
-      .addAttribute("ram_style", params.bgAttrsArrRamStyle)
+    //val bgAttrsMem = Mem(
+    //  wordType=Gpu2dBgAttrs(params=params),
+    //  wordCount=params.numBgs,
+    //)
+    //  .initBigInt(Array.fill(params.numBgs)(BigInt(0)).toSeq)
+    //  .addAttribute("ram_style", params.bgAttrsArrRamStyle)
     //--------
     val objAttrsMem = Mem(
       wordType=Gpu2dObjAttrs(params=params),
@@ -402,6 +469,12 @@ case class Gpu2d(
     )
       .initBigInt(Array.fill(params.numObjs)(BigInt(0)).toSeq)
       .addAttribute("ram_style", params.objAttrsArrRamStyle)
+    when (objAttrsPush.fire) {
+      objAttrsMem.write(
+        address=objAttrsPush.payload.memIdx,
+        data=objAttrsPush.payload.objAttrs,
+      )
+    }
     //--------
     val palEntryMem = Mem(
       wordType=Gpu2dPalEntry(params=params),
@@ -409,15 +482,28 @@ case class Gpu2d(
     )
       .initBigInt(Array.fill(params.numColsInPal)(BigInt(0)).toSeq)
       .addAttribute("ram_style", params.palEntryArrRamStyle)
+    when (palEntryPush.fire) {
+      palEntryMem.write(
+        address=palEntryPush.payload.memIdx,
+        data=palEntryPush.payload.palEntry,
+      )
+    }
     //--------
-    //val lineMem = Mem(
-    //  wordType=Rgb(params.rgbConfig),
-    //  wordCount=params.lineMemSize,
-    //)
-    //  .initBigInt(Array.fill(params.lineMemSize)(BigInt(0)).toSeq)
-    //  .addAttribute("ram_style", params.lineArrRamStyle)
-    //val nextLineIdx = KeepAttribute(UInt(1 bits))
-    //val rLineIdx = KeepAttribute(RegNext(nextLineIdx)) init(0x0)
+    val lineMemArr = new ArrayBuffer[Mem[Rgb]]()
+
+    for (idx <- 0 to params.numLineMems - 1) {
+      lineMemArr += Mem(
+        wordType=Rgb(params.rgbConfig),
+        wordCount=params.lineMemSize,
+      )
+        .initBigInt(Array.fill(params.lineMemSize)(BigInt(0)).toSeq)
+        .addAttribute("ram_style", params.lineArrRamStyle)
+    }
+    val nextLineIdx = KeepAttribute(UInt(log2Up(params.numLineMems) bits))
+    val rLineIdx = KeepAttribute(RegNext(nextLineIdx)) init(0x0)
+    //val nextCol = Rgb(params.rgbConfig)
+    //val rCol = RegNext(nextCol)
+    //rCol.init(rCol.getZero)
     //--------
     //--------
   }
