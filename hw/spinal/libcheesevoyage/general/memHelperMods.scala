87b0fa09dceb43c7522b51deaf1b3d48f0e2ce09
fl4shk <fl4shk@users.noreply.github.com>
2025-06-26 10:51:56 -0500

    correctness debugging

--- hw/spinal/libcheesevoyage/general/memHelperMods.scala
+++ hw/spinal/libcheesevoyage/general/memHelperMods.scala
@@ -55,7 +55,7 @@ case class RamSimpleDualPortWriteFirst[
     /*RegNext*/(
       //RegNext(io.rdAddr) === io.wrAddr
       io.cmpRdWrAddrEtc
-      //&& RegNext(io.ramIo.rdEn, init=False)
+      && RegNext(io.ramIo.rdEn, init=False)
       && io.ramIo.wrEn
     )
     //init(False)
