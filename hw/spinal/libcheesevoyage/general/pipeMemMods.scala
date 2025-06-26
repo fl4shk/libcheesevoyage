3f831b31df9444b7b14f08cf50cbc41d4982a1e2
fl4shk <fl4shk@users.noreply.github.com>
2025-06-26 18:07:22 -0500

    fmax debugging

--- hw/spinal/libcheesevoyage/general/pipeMemMods.scala
+++ hw/spinal/libcheesevoyage/general/pipeMemMods.scala
@@ -2302,13 +2302,13 @@ extends Area {
                   //currMemAddr(0)
                   currMemAddr === prevMemAddr
                 ) && (
-                  prev.modMemWordValid(
-                    if (zdx < prev.modMemWordValid.size) (
-                      zdx
-                    ) else (
-                      prev.modMemWordValid.size - 1
-                    )
-                  )
+                  //prev.modMemWordValid(
+                  //  if (zdx < prev.modMemWordValid.size) (
+                  //    zdx
+                  //  ) else (
+                  //    prev.modMemWordValid.size - 1
+                  //  )
+                  //)
                 )
               )
               //else (
