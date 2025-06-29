7bd00ac5d5a3f91881b42e48d6cc64c2addca90b
fl4shk <fl4shk@users.noreply.github.com>
2025-06-29 15:19:14 -0500

    fmax/correctness debugging

--- hw/spinal/libcheesevoyage/general/pipeMemMods.scala
+++ hw/spinal/libcheesevoyage/general/pipeMemMods.scala
@@ -4008,17 +4008,18 @@ extends Area {
                       )
                     ) && (
                       myZeroRegCond
-                    ) && (
-                      tempMyUpExtDel.modMemWordValid({
-                        if (
-                          idx < tempMyUpExtDel.modMemWordValid.size
-                        ) (
-                          idx
-                        ) else (
-                          tempMyUpExtDel.modMemWordValid.size - 1 
-                        )
-                      })
                     )
+                    //&& (
+                    //  tempMyUpExtDel.modMemWordValid({
+                    //    if (
+                    //      idx < tempMyUpExtDel.modMemWordValid.size
+                    //    ) (
+                    //      idx
+                    //    ) else (
+                    //      tempMyUpExtDel.modMemWordValid.size - 1 
+                    //    )
+                    //  })
+                    //)
                     //&& (
                     //  tempMyUpExtDel.valid(0)
                     //)
@@ -4656,13 +4657,13 @@ extends Area {
             )
           )
         }
-        //when (
-        //  up.isValid
-        //  //up.isFiring
-        //  //True
-        //) {
+        when (
+          up.isValid
+          //up.isFiring
+          //True
+        ) {
           upExt(1)(ydx)(extIdxUp) := upExt(0)(ydx)(extIdxSingle)
-        //}
+        }
         if (ydx == 0) {
           if (myHaveFormalFwd) {
             tempUpMod(0).formalGetPipeMemRmwFwd(
@@ -4853,12 +4854,12 @@ extends Area {
           )
         )
       }
-      //when (
-      //  up.isValid
-      //  //up.isFiring
-      //) {
+      when (
+        up.isValid
+        //up.isFiring
+      ) {
         upExt(1)(ydx)(extIdxUp) := upExt(0)(ydx)(extIdxSingle)
-      //}
+      }
       if (ydx == 0) {
         if (myHaveFormalFwd) {
           tempUpMod(0).formalGetPipeMemRmwFwd(
@@ -5273,7 +5274,7 @@ extends Area {
       mkExt()
     )
     for (ydx <- 0 until memArrSize) {
-      if (optModHazardKind != PipeMemRmw.ModHazardKind.Fwd) {
+      //if (optModHazardKind != PipeMemRmw.ModHazardKind.Fwd) {
         for (extIdx <- 0 until extIdxLim) {
           upExt(1)(ydx)(extIdx) := (
             RegNext(
@@ -5289,9 +5290,9 @@ extends Area {
         ) {
           upExt(1)(ydx)(extIdxUp) := upExt(0)(ydx)(extIdxSingle)
         }
-      } else {
-        upExt(1)(ydx)(extIdxUp) := upExt(0)(ydx)(extIdxSingle)
-      }
+      //} else {
+      //  upExt(1)(ydx)(extIdxUp) := upExt(0)(ydx)(extIdxSingle)
+      //}
       //val tempHadActiveUpFire = Bool()
       //when (
       //  //down.isFiring
