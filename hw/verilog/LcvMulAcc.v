571425005f2a97ff632554284348f73eb380b514
fl4shk <fl4shk@users.noreply.github.com>
2025-07-02 16:51:45 -0500

    correctness debugging

--- hw/verilog/LcvMulAcc.v
+++ hw/verilog/LcvMulAcc.v
@@ -2,10 +2,10 @@
 module LcvMulAcc32(
 	input wire signed [26:0] a,
 	input wire signed [17:0] b,
-	input wire signed [31:0] c,
-	input wire signed [31:0] d,
-	input wire signed [31:0] e,
-	output wire signed [31:0] outp
+	input wire signed [47:0] c,
+	input wire signed [47:0] d,
+	input wire signed [47:0] e,
+	output wire signed [47:0] outp
 );
 	wire signed [47:0] pcout;
 	assign pcout = a * b + c;
