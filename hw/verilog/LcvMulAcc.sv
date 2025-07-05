28bfa2f1ca979021d4254d1927e6c662bfcbfe03
fl4shk <fl4shk@users.noreply.github.com>
2025-07-05 18:44:33 -0500

    correctness debugging

--- hw/verilog/LcvMulAcc.sv
+++ hw/verilog/LcvMulAcc.sv
@@ -88,8 +88,8 @@ module LcvCmpEqDel1 #(
 	input logic signed [WIDTH - 1:0] b,
 	output logic signed [WIDTH:0] outp_data
 );
-	wire signed [WIDTH:0] my_a = $signed({2'b0, a});
-	wire signed [WIDTH:0] my_b = $signed({2'b0, b});
+	wire signed [WIDTH:0] my_a = $signed({1'b0, a});
+	wire signed [WIDTH:0] my_b = $signed({1'b0, b});
 	wire signed [WIDTH:0] my_carry_in = (
 		$signed({{(WIDTH){1'b0}}, 1'b1})
 	);
