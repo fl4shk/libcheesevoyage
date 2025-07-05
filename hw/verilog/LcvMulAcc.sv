41dd7c147562e414b40e923a4c39a3aed4282819
fl4shk <fl4shk@users.noreply.github.com>
2025-07-05 18:30:19 -0500

    debugging

--- hw/verilog/LcvMulAcc.sv
+++ hw/verilog/LcvMulAcc.sv
@@ -86,11 +86,11 @@ module LcvCmpEqDel1 #(
 	input logic clk,
 	input logic signed [WIDTH - 1:0] a,
 	input logic signed [WIDTH - 1:0] b,
-	output logic signed [WIDTH:0] outp_data
+	output logic signed [WIDTH + 1:0] outp_data
 );
-	wire signed [WIDTH:0] my_a = {1'b0, a};
-	wire signed [WIDTH:0] my_b = {1'b0, b};
-	wire signed [WIDTH:0] my_carry_in = {{WIDTH{1'b0}}, 1'b1};
+	wire signed [WIDTH + 1:0] my_a = {2'b0, a};
+	wire signed [WIDTH + 1:0] my_b = {2'b0, b};
+	wire signed [WIDTH + 1:0] my_carry_in = {{(WIDTH + 1){1'b0}}, 1'b1};
 	always @(posedge clk) begin
 		outp_data <= my_a ^ (~my_b) + my_carry_in;
 		//outp_sum <= a + b;
