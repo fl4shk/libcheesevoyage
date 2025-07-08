b8586e8f1758c2bcb020bcd642cbcb00689fc81f
fl4shk <fl4shk@users.noreply.github.com>
2025-07-08 10:21:14 -0500

    `PipeMemRmw`: correctness debugging

--- hw/spinal/libcheesevoyage/general/pipeMemMods.scala
+++ hw/spinal/libcheesevoyage/general/pipeMemMods.scala
@@ -4138,7 +4138,7 @@ extends Area {
         val myPreFwdArea = (
           mkPreFwdArea(
             upIsFiring=up.isFiring,
-            upExtElem=upExt(2),
+            upExtElem=upExt(1),
           )
         )
       }
