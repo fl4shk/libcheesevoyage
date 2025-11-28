c69578f82eb3ee8f42061f1bdc182a4ae2046fa5
fl4shk <fl4shk@users.noreply.github.com>
2025-11-28 11:25:11 -0600

    for some reason, the state machine was ignored or some such?

--- hw/spinal/libcheesevoyage/bus/lcvStall/lcvStallBusSdramCtrlMod.scala
+++ hw/spinal/libcheesevoyage/bus/lcvStall/lcvStallBusSdramCtrlMod.scala
@@ -612,7 +612,7 @@ case class LcvStallBusSdramCtrl(
     )
   )
 
-  object State extends SpinalEnum(defaultEncoding=binaryOneHot) {
+  object State extends SpinalEnum(defaultEncoding=binarySequential) {
     val
       PWR_ON_INIT,
       PWR_ON_CNT_DO_CKE_HI,
