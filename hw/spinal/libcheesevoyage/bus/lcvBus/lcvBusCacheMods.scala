74dae1f032f67726f66dc1a6d4ee9e960431326e
fl4shk <fl4shk@users.noreply.github.com>
2026-03-12 18:45:51 -0500

    correctness debugging (it's not correct *yet*)

--- hw/spinal/libcheesevoyage/bus/lcvBus/lcvBusCacheMods.scala
+++ hw/spinal/libcheesevoyage/bus/lcvBus/lcvBusCacheMods.scala
@@ -1514,10 +1514,7 @@ private[libcheesevoyage] case class LcvBusNonCoherentInstrCache(
           when (!myLoD2hStm.fire) {
             //base.myFifoThingDoStall.last := True
             base.myFifoThingDoStall := True
-            //loH2dPopStm.ready := False
-            //loH2dPopStm.ready := (
-            //  
-            //)
+            loH2dPopStm.ready := False
             //base.myFifoThingDoStall := True
             rState := State.LOAD_HIT_LO_BUS_STALL
           }
@@ -1563,9 +1560,9 @@ private[libcheesevoyage] case class LcvBusNonCoherentInstrCache(
       //--------
     }
     is (State.LOAD_HIT_LO_BUS_STALL) {
-      //when (rose(rState === State.LOAD_HIT_LO_BUS_STALL)) {
-      //  loH2dPopStm.ready := True
-      //}
+      when (rose(rState === State.LOAD_HIT_LO_BUS_STALL)) {
+        loH2dPopStm.ready := True
+      }
       myLoD2hStm.valid := True
       when (myLoD2hStm.fire) {
         //base.myFifoThingDoStall.last := False
