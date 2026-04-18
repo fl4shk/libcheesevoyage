2ffc0693aee825798a3421cd77107aac6c8d5977
fl4shk <fl4shk@users.noreply.github.com>
2026-04-18 15:46:31 -0500

    correctness debugging

--- hw/spinal/libcheesevoyage/general/pipeRegFileAreas.scala
+++ hw/spinal/libcheesevoyage/general/pipeRegFileAreas.scala
@@ -1921,53 +1921,52 @@ extends Area {
               //forFwd
               cfg.optModHazardKind == PipeRegFile.ModHazardKind.Fwd
             ) (
-              (
-                if (idx == 0 || idx == 1) (
+              if (idx == 0 || idx == 1) (
+                currMemAddr(0)
+                //&& (
+                //  prev.modMemWordValid(
+                //    if (zdx < prev.modMemWordValid.size) (
+                //      zdx
+                //    ) else (
+                //      prev.modMemWordValid.size - 1
+                //    )
+                //  )
+                //)
+              ) else (
+                (
                   currMemAddr(0)
-                  //&& (
-                  //  prev.modMemWordValid(
-                  //    if (zdx < prev.modMemWordValid.size) (
-                  //      zdx
-                  //    ) else (
-                  //      prev.modMemWordValid.size - 1
-                  //    )
-                  //  )
-                  //)
-                ) else (
-                  (
-                    currMemAddr(0)
-                    //currMemAddr === prevMemAddr
-                  )
-                  && (
-                    prev.modMemWordValid(
-                      if (zdx < prev.modMemWordValid.size) (
-                        zdx
-                      ) else (
-                        prev.modMemWordValid.size - 1
-                      )
+                  //currMemAddr === prevMemAddr
+                )
+                && (
+                  prev.modMemWordValid(
+                    if (zdx < prev.modMemWordValid.size) (
+                      zdx
+                    ) else (
+                      prev.modMemWordValid.size - 1
                     )
                   )
                 )
-                //else (
-                //  cfg.optFwdHaveZeroReg match {
-                //    case Some(myZeroRegIdx) => {
-                //      currMemAddr === prevMemAddr
-                //    }
-                //    case None => {
-                //      (
-                //        currMemAddr === prevMemAddr
-                //      ) && (
-                //        prev.modMemWordValid(
-                //          if (zdx < prev.modMemWordValid.size) (
-                //            zdx
-                //          ) else (
-                //            prev.modMemWordValid.size - 1
-                //          )
-                //        )
-                //      )
-                //    }
-                //  }
-                //)
+              )
+              //else (
+              //  cfg.optFwdHaveZeroReg match {
+              //    case Some(myZeroRegIdx) => {
+              //      currMemAddr === prevMemAddr
+              //    }
+              //    case None => {
+              //      (
+              //        currMemAddr === prevMemAddr
+              //      ) && (
+              //        prev.modMemWordValid(
+              //          if (zdx < prev.modMemWordValid.size) (
+              //            zdx
+              //          ) else (
+              //            prev.modMemWordValid.size - 1
+              //          )
+              //        )
+              //      )
+              //    }
+              //  }
+              //)
             ) else (
               (
                 if (!isPostDelay) (
