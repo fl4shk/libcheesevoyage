143b040dddf8ab92e4aa71bdb203a518b6386854
fl4shk <fl4shk@users.noreply.github.com>
2026-06-20 15:48:44 -0500

    more correctness debugging yay!

--- hw/spinal/libcheesevoyage/bus/lcvBus/lcvBusCacheMods.scala
+++ hw/spinal/libcheesevoyage/bus/lcvBus/lcvBusCacheMods.scala
@@ -1321,7 +1321,7 @@ case class LcvBusDoStallH2dReptThing(
       io.pop.payload := rSavedLoH2dPopInfoVec.head.payload
       when (io.pop.fire) {
         rSavedLoH2dPopInfoVec.head.valid := False
-        rPrevRewriteIdx.lsb := False//True
+        //rPrevRewriteIdx.lsb := False//True
         rState := State.MAIN
       }
     }
@@ -1330,7 +1330,7 @@ case class LcvBusDoStallH2dReptThing(
       io.pop.payload := rSavedLoH2dPopInfoVec.last.payload
       when (io.pop.fire) {
         rSavedLoH2dPopInfoVec.last.valid := False
-        rPrevRewriteIdx.lsb := False//True
+        //rPrevRewriteIdx.lsb := False//True
         rState := State.MAIN
       }
     }
