9c7d12d18611c9f820e835288007b5d64de1580e
fl4shk <fl4shk@users.noreply.github.com>
2026-07-14 11:03:31 -0500

    fmax debugging

--- hw/spinal/libcheesevoyage/general/memHelperMods.scala
+++ hw/spinal/libcheesevoyage/general/memHelperMods.scala
@@ -994,7 +994,7 @@ case class WrPulseRdPipeRamSdpPipe[
   val sLastBackArr = new ArrayBuffer[StageLink]()
   val s2mLastBackArr = new ArrayBuffer[S2MLink]()
   for (
-    idx <- 0 until 1//2
+    idx <- 0 until 2//1//2
   ) {
     if (idx == 0) {
       sLastBackArr += StageLink(
