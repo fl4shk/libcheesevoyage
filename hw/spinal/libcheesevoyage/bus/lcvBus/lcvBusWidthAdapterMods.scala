498f0c502c9c8b97bcbcbac7ddd5c9d7638be9be
fl4shk <fl4shk@users.noreply.github.com>
2026-08-14 19:16:30 -0500

    can-build/correctness debugging

--- hw/spinal/libcheesevoyage/bus/lcvBus/lcvBusWidthAdapterMods.scala
+++ hw/spinal/libcheesevoyage/bus/lcvBus/lcvBusWidthAdapterMods.scala
@@ -141,7 +141,7 @@ case class LcvBusSimpleReadBurstOnlyDataWidthAdapter(
       io.hiBus.h2dBus.addr := rSavedLoH2dPayload.addr
       io.hiBus.h2dBus.src := rSavedLoH2dPayload.src
       if (io.hiBus.h2dBus.mainNonBurstInfo.infoByteEn != null) {
-        io.hiBus.h2dBus.byteEn := rSavedLoH2dPayload.byteEn
+        io.hiBus.h2dBus.byteEn := 0x0//rSavedLoH2dPayload.byteEn
       }
       if (io.hiBus.h2dBus.mainNonBurstInfo.infoByteSizeEtc != null) {
         io.hiBus.h2dBus.byteSize := rSavedLoH2dPayload.byteSize
