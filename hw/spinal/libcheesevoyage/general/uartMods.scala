6011081c01fef5850a80a8ead912725aa3579565
fl4shk <fl4shk@users.noreply.github.com>
2026-09-01 19:17:40 -0500

    UART Tx: commit to save my progress, but needs more work!

--- hw/spinal/libcheesevoyage/general/uartMods.scala
+++ hw/spinal/libcheesevoyage/general/uartMods.scala
@@ -24,19 +24,65 @@ case class LcvUartCtrlConfig(
   val cntWidth = 1 + log2Up(cyclesPerBit)
 }
 
+case class LcvUartCtrlTxMostIo(
+  cfg: LcvUartCtrlConfig
+) extends Bundle with IMasterSlave {
+  val tx = in(Bool())
+    // the payload line (the serialized data word and start/stop bits)
+
+  val dtr = in(Bool())
+    // Data Terminal Ready
+    // Driven by the TX.
+
+  val dsr = out(Bool())
+    // Data Set Ready
+    // The mirror of DTR. Asserted when the DCE is powered on and ready.
+    // Driven by the RX.
+
+  val rts = in(Bool())
+    // Request to Send
+    // TX's "I'm ready for data."
+    // Driven by the TX.
+
+  val cts = out(Bool())
+    // Clear to Send
+    // RX's "I'm ready for data."
+    // Driven by the RX.
+
+  //val dcd = out(Bool())
+  //  // Data Carrier Detect
+  //  // On non-modem links, DCD is often forced high by jumpering it to DSR
+  //  // Or ignored entirely
+  //  // Driven by the RX.
+
+  def asMaster(): Unit = {
+    out(tx)
+    out(dtr)
+    in(dsr)
+    out(rts)
+    in(cts)
+    //in(dcd)
+  }
+}
 case class LcvUartCtrlTxIo(
   cfg: LcvUartCtrlConfig,
 ) extends Bundle with IMasterSlave {
-  val tx = in(Bool())
-  //val rx = in(Bool())
+  val most = slave(
+    LcvUartCtrlTxMostIo(cfg=cfg)
+  )
+  def tx = most.tx
+  def dtr = most.dtr
+  def dsr = most.dsr
+  def rts = most.rts
+  def cts = most.cts
 
-  val push = master(Stream(
-    UInt(cfg.wordWidth bits)
-  ))
-  //val pop = master(Stream(
-  //))
+  val push = master(
+    // what data do we send?
+    Stream(UInt(cfg.wordWidth bits))
+  )
+
   def asMaster(): Unit = {
-    out(tx)
+    master(most)
     slave(push)
   }
 }
@@ -45,7 +91,7 @@ case class LcvUartCtrlTx(
   cfg: LcvUartCtrlConfig,
 ) extends Component {
   //--------
-  val io = slave(
+  val io = master(
     LcvUartCtrlTxIo(cfg=cfg)
   )
   //--------
