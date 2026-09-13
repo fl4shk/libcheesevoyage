3cb45cdd56096cb9cff7e50a3758a50c93f0b24f
fl4shk <fl4shk@users.noreply.github.com>
2026-09-13 12:39:31 -0500

    correctness debugging

--- hw/spinal/libcheesevoyage/bus/lcvBus/lcvBusCacheMods.scala
+++ hw/spinal/libcheesevoyage/bus/lcvBus/lcvBusCacheMods.scala
@@ -6307,7 +6307,7 @@ private[libcheesevoyage] case class LcvBusInstrCacheMain(
     }
     is (LoState.WAIT_HI_STATE_IDLE) {
       when (rHiState === HiState.IDLE) {
-        rLoState := LoState.IDLE
+        rLoState := LoState.WAIT_HI_STATE_IDLE_POST_7
       }
     }
     is (LoState.WAIT_HI_STATE_IDLE_POST_7) {
